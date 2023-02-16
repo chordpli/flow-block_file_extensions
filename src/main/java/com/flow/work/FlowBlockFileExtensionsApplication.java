fbe7a32cbae71c7e873fff2049b5d18936189329
chordpli <chordplaylist@gmail.com>
2023-02-16 16:28:28 +0900

    build: build project

---
+++ src/main/java/com/flow/work/FlowBlockFileExtensionsApplication.java
@@ -0,0 +1,13 @@
+package com.flow.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowBlockFileExtensionsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlowBlockFileExtensionsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/flow/work/FlowBlockFileExtensionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowBlockFileExtensionsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
