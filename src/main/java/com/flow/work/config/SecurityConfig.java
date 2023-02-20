6c65fe572ced5faa2977b233922bc3ab0e049f68
chordpli <chordplaylist@gmail.com>
2023-02-20 10:12:19 +0900

    feat: config
    1. config 수정

--- src/main/java/com/flow/work/config/SecurityConfig.java
+++ src/main/java/com/flow/work/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.flow.work.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,11 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
   private final String PERMIT = "/**";
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity.
@@ -20,6 +23,8 @@ public class SecurityConfig {
         .cors().disable()
         .authorizeRequests()
         .antMatchers(PERMIT).permitAll()
+        .and()
+        .exceptionHandling()
         .and()
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
